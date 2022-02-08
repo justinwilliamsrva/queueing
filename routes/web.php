3a9f4f3eeccef7edb3b599d31223a36578987f21
Justin Williams <justinwilliams2@me.com>
2022-02-08 15:24:31 -0500

    changed queue to redis

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    dispatch(function () {
+        logger("Running");
+    });
+
+
+    return "Done";
 });
